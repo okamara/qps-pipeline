13fa5f8d147583ee26253532c3ce0c0dda45765d
tamikana <tkantarovich@qaprosoft.com>
2019-01-28 16:32:11 +0300

    add sbt runner

---
+++ src/com/qaprosoft/jenkins/pipeline/sbt/SBTRunner.groovy
@@ -0,0 +1,65 @@
+package com.qaprosoft.jenkins.pipeline.sbt
+
+import com.qaprosoft.Utils
+import com.qaprosoft.jenkins.pipeline.Configuration
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+class SBTRunner extends AbstractRunner {
+
+    public SBTRunner(context) {
+        super(context)
+        scmClient = new GitHub(context)
+
+        currentBuild = context.currentBuild
+    }
+
+    @Override
+    protected void build() {
+        logger.info("SBTRunner->runJob")
+        context.node("performance") {
+
+            context.wrap([$class: 'BuildUser']) {
+                try {
+                    context.timestamps {
+
+                        context.env.getEnvironment()
+
+                        scmClient.clone()
+
+                        def sbtHome = tool 'SBT'
+
+                        context.timeout(time: Integer.valueOf(Configuration.get(Configuration.Parameter.JOB_MAX_RUN_TIME)), unit: 'MINUTES') {
+                            context.sh "${sbtHome} ${args}"
+                        }
+
+                    }
+                } catch (Exception e) {
+                    logger.error(Utils.printStackTrace(e))
+                    throw e
+                } finally {
+                    publishJenkinsReports()
+                    clean()
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPush(){
+        //TODO: implement in future
+    }
+
+    @Override
+    public void onPullRequest(){
+        //TODO: implement in future
+    }
+
+    protected void publishJenkinsReports() {
+        context.stage('Results') {
+            context.gatlingArchive()
+        }
+    }
+
+
+}

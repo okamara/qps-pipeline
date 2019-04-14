ce9380169c93d56a16c54256c5e0b32783185a0f
tamikana <tkantarovich@qaprosoft.com>
2019-04-14 14:52:05 +0300

    add public

--- src/com/qaprosoft/jenkins/pipeline/sbt/AbstractSBTRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/sbt/AbstractSBTRunner.groovy
@@ -8,10 +8,10 @@ import java.text.SimpleDateFormat
 
 public abstract class AbstarctSBTRunnner extends AbstractRunner {
 
-    def date = new Date()
-    def sdf = new SimpleDateFormat("yyyyMMddHHmmss")
-    String curDate = sdf.format(date)
-    String randomCompareArchiveName = "loadTestingReports" + curDate + ".zip"
+    public def date = new Date()
+    public def sdf = new SimpleDateFormat("yyyyMMddHHmmss")
+    public String curDate = sdf.format(date)
+    public String randomCompareArchiveName = "loadTestingReports" + curDate + ".zip"
 
     public AbstarctSBTRunnner(context) {
         super(context)

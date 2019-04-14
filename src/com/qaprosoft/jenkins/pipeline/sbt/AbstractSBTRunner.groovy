ae54f89a7ee8b6590a077d4b29f6742ca0c9f3b9
tamikana <tkantarovich@qaprosoft.com>
2019-04-14 16:30:56 +0300

    f13

--- src/com/qaprosoft/jenkins/pipeline/sbt/AbstractSBTRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/sbt/AbstractSBTRunner.groovy
@@ -10,12 +10,6 @@ import com.qaprosoft.jenkins.pipeline.AbstractRunner
 @InheritConstructors
 public class AbstarctSBTRunnner extends AbstractRunner{
 
-    public AbstarctSBTRunnner(context) {
-        super(context)
-        scmClient = new GitHub(context)
-        this.context=context
-    }
-
     protected def date = new Date()
     protected def sdf = new SimpleDateFormat("yyyyMMddHHmmss")
     protected String curDate = sdf.format(date)
